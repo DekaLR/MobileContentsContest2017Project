71b628abb2a1640c147d7cf54a5f5e6b2ab431dc
sunrin <deka5224@gmail.com>
2017-06-25 15:29:55 +0900

    V.000000001

--- app/src/main/java/mobile/contents/contest/MainActivity.java
+++ app/src/main/java/mobile/contents/contest/MainActivity.java
@@ -89,8 +89,6 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-
         int id = item.getItemId();
 
         Fragment fragment = null;
